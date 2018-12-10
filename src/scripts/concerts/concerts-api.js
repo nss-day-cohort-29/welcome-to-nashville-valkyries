f281076dda792eb11899695c565db75c487a36ef
Russell Reiter <russell.reiter@gmail.com>
2018-12-10 15:27:50 -0600

    Update variable handling and refactored code

--- src/scripts/concerts/concerts-api.js
+++ src/scripts/concerts/concerts-api.js
@@ -1,25 +1,17 @@
-let today = new Date();
-let searchDate = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
-let concertsSearchParam = "music"//eventListeners.concertsSearchParam;
-let parsedConcerts = [];
+function concertsFetch () {
+        
+        let today = new Date();
+        let searchDate = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
+        let parsedConcerts = [];
 
-function concertsFetch (concertsSearchParam) {fetch
-(`https://app.ticketmaster.com/discovery/v2/events.json?dmaId=343&classificationName=${concertsSearchParam}&localStartDateTime=${searchDate}T00:00:01,${searchDate}T23:59:59&apikey=gOoLh8VX4xnv0GAbvCblAvu8bCKOVE95`)
+        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?dmaId=343&classificationName=${concertsSearchParam}&localStartDateTime=${searchDate}T00:00:01,${searchDate}T23:59:59&apikey=gOoLh8VX4xnv0GAbvCblAvu8bCKOVE95`)
         .then (response => response.json())
         .then (concertInfo => {
 
                 concertInfo._embedded.events.forEach(concert => {
-                        let eventName = concert.name;
-                        let genre = concert.classifications[0].genre.name;
-                        let venue = concert._embedded.venues[0].name;
-                        let event = `${eventName}, at ${venue}, Genre: ${genre}`;
+                        let event = `${concert.name}, at ${concert._embedded.venues[0].name}, Genre: ${concert.classifications[0].genre.name}`;
                         parsedConcerts.push(event)
                 })
-
-                return parsedConcerts;
         });
-        console.log(parsedConcerts);
-        //domBuilder.domCompBuilder(parsedConcerts);
+        domBuilder.domCompBuilder(parsedConcerts);
 };
-
-concertsFetch();
