0d75fc5f27b115e41672f0969f639a1c9c4a3f47
Russell Reiter <russell.reiter@gmail.com>
2018-12-11 16:08:00 -0600

    Reset concerts array for each new search

--- src/scripts/concerts/concerts-api.js
+++ src/scripts/concerts/concerts-api.js
@@ -8,7 +8,7 @@ function concertsFetch (concertsSearchParam) {
         fetch(`https://app.ticketmaster.com/discovery/v2/events.json?dmaId=343&classificationName=${concertsSearchParam}&localStartDateTime=${searchDate}T00:00:01,${searchDate}T23:59:59&apikey=gOoLh8VX4xnv0GAbvCblAvu8bCKOVE95`)
         .then (response => response.json())
         .then (concertInfo => {
-
+                parsedConcerts = ["concerts"];
                 concertInfo._embedded.events.forEach(concert => {
                         let event = `${concert.name}, at ${concert._embedded.venues[0].name}, Genre: ${concert.classifications[0].genre.name}`;
                         parsedConcerts.push(event)
