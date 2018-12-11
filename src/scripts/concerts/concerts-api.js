ec0e9e646dcd05f6cca0d435283332a10e557caa
Russell Reiter <russell.reiter@gmail.com>
2018-12-11 12:08:45 -0600

    Concerts final

--- src/scripts/concerts/concerts-api.js
+++ src/scripts/concerts/concerts-api.js
@@ -1,10 +1,8 @@
-
 function concertsFetch (concertsSearchParam) {
         
         let today = new Date();
         let searchDate = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
-        let parsedConcerts = [];
-        // let concertsSearchParam = "music"; //remove once eventListeners.js is providing variable.
+        let parsedConcerts = ["concerts"];
 
         fetch(`https://app.ticketmaster.com/discovery/v2/events.json?dmaId=343&classificationName=${concertsSearchParam}&localStartDateTime=${searchDate}T00:00:01,${searchDate}T23:59:59&apikey=gOoLh8VX4xnv0GAbvCblAvu8bCKOVE95`)
         .then (response => response.json())
@@ -14,15 +12,6 @@ function concertsFetch (concertsSearchParam) {
                         let event = `${concert.name}, at ${concert._embedded.venues[0].name}, Genre: ${concert.classifications[0].genre.name}`;
                         parsedConcerts.push(event)
                 });
-                let domFragReturn = domComponents.createResourcesDocumentFragments(parsedConcerts);
-                        console.log(domFragReturn);
-                        domBuilder.appendAllResources(domFragReturn);
-                // return parsedConcerts;
-                
+                domBuilder.appendAllResources(domComponents.createResourcesDocumentFragments(parsedConcerts));
         });
-        // console.log(parsedConcerts);
-        // domBuilder.domCompBuilder(parsedConcerts);
 };
-
-// concertsFetch();
-
