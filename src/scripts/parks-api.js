ebae3bd8702a2cae057e7dce908c54df2e7fabee
alagrad94 <36314037+alagrad94@users.noreply.github.com>
2018-12-11 13:36:56 -0600

    Update parks-api.js

--- src/scripts/parks-api.js
+++ src/scripts/parks-api.js
@@ -1,6 +1,6 @@
 // // This module interacts with the API, meaning all fetch calls to the json-server API will be in this module.
 
-  let parksArray = [];
+  let parksArray = ["parks"];
      function parksFetch (parksSearchParam) {
       fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=mGIe1HeHEMg9aF55Guy8rdqhN&${parksSearchParam}=Yes`)
           .then(response => response.json())
