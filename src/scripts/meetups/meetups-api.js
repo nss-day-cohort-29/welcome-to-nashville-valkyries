90ef94c8c4fcb58d685ddad56482632766d4bcc0
colebryant <cobryant47@gmail.com>
2018-12-11 09:07:00 -0600

    in process adding venue name

--- src/scripts/meetups/meetups-api.js
+++ src/scripts/meetups/meetups-api.js
@@ -1,27 +1,58 @@
 
 let eventsArray = [];
 
-const meetupsObject = {
-    meetUpsFetch(meetUpsSearchParam) {
-    return fetch(`https://www.eventbriteapi.com/v3/events/search/?location.latitude=36.174465&location.longitude=-86.767960&start_date.keyword=today&q=${meetUpsSearchParam}`, {
-        headers: {"Authorization": "Bearer Z7Y4SSESJQECHYEKDADA"}})
-        .then(response => response.json())
-        .then(response => {
-            response.events.forEach(event => {
-                let eventName = event.name.text;
-                // let eventVenue = () => {
-                //     return fetch(`https://www.eventbriteapi.com/v3/venues/${event.venue_id}/`)
-                //         .then(venueResponse => venueResponse.json())
-                //         .then(venueResponse => {
-                //         return venueResponse.name;
-                //         })
-                // }
-                eventsArray.push(eventName);
-            });
-        })
-        //domBuilder.domCompBuilder(eventsArray);
-}};
+// const meetupsObject = {
+//     meetUpsFetch(meetUpsSearchParam) {
+//     return fetch(`https://www.eventbriteapi.com/v3/events/search/?location.latitude=36.174465&location.longitude=-86.767960&start_date.keyword=today&q=${meetUpsSearchParam}`, {
+//         headers: {"Authorization": "Bearer Z7Y4SSESJQECHYEKDADA"}})
+//         .then(response => response.json())
+//         .then(response => {
+//             response.events.forEach(event => {
+//                 let eventName = event.name.text;
+//                 eventsArray.push(eventName);
+//             });
+//         })
+//         //domBuilder.domCompBuilder(eventsArray);
+// }};
 
-meetupsObject.meetUpsFetch("Music");
+// meetupsObject.meetUpsFetch("Music");
 
-console.log(eventsArray);
+// console.log(eventsArray);
+
+// SECTION FOR VENUE NAME TO BE IMPLEMENTED
+
+// fetch(`https://www.eventbriteapi.com/v3/events/search/?location.latitude=36.174465&location.longitude=-86.767960&start_date.keyword=today&q=Music`, {
+//     headers: {"Authorization": "Bearer Z7Y4SSESJQECHYEKDADA"}})
+//     .then(response => response.json())
+//     .then(response => {
+//         response.events.forEach(event => {
+//             let eventName = event.name.text;
+//             let eventVenue = () => {
+//                 return fetch(`https://www.eventbriteapi.com/v3/venues/${event.venue_id}/`, {
+//                     headers: {"Authorization": "Bearer Z7Y4SSESJQECHYEKDADA"}})
+//                     .then(venueResponse => venueResponse.json())
+//                     .then(venueResponse => {
+//                     return venueResponse.name;
+//                     })
+//             };
+//             eventsArray.push(`${eventName}, Location: ${eventVenue()}`);
+//         })
+//     });
+
+// console.log(eventsArray);
+
+let thing = "";
+
+const fooFunction = () => {
+    fetch("https://www.eventbriteapi.com/v3/venues/28111554/?token=Z7Y4SSESJQECHYEKDADA")
+    .then(venueResponse => venueResponse.json())
+    .then(venueResponse => {
+        venueResponse.forEach(venue => {
+            thing = venue.name;
+        });
+    });
+};
+
+console.log(thing);
+
+fooFunction();
